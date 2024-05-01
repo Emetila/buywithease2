52edca8bb98ba7a4d05d5020cb7df6c7a6483df0
Emetila <emetila83@gmail.com>
2024-05-01 18:55:53 +0100

    Implemented products in product page

---
+++ src/App/components/BagProducts/product.js
@@ -0,0 +1,86 @@
+import bag1 from '../../assets/Image/bags/bag1.svg';
+import bag2 from '../../assets/Image/bags/bag2.svg';
+import bag3 from '../../assets/Image/bags/bag3.svg';
+import bag4 from '../../assets/Image/bags/bag4.svg';
+import bag5 from '../../assets/Image/bags/bag5.svg';
+import bag6 from '../../assets/Image/bags/bag6.svg';
+import bag7 from '../../assets/Image/bags/bag7.svg';
+import bag8 from '../../assets/Image/bags/bag8.svg';
+import bag9 from '../../assets/Image/bags/bag9.svg';
+import bag10 from '../../assets/Image/bags/bag10.svg';
+import bag11 from '../../assets/Image/bags/bag11.svg';
+import bag12 from '../../assets/Image/bags/bag12.svg';
+import star from '../../assets/Icons/star.svg'
+
+export const bagProduct = [{
+    Image: bag1,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star,
+},
+{
+    Image: bag2,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag3,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag4,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag5,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag6,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag7,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag8,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag9,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag10,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag11,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+},
+{
+    Image: bag12,
+    title: "Premium Handbags Collection",
+    amount: "₦40,599.99",
+    rating: star
+}]
--- src/App/pages/Product/index.jsx
+++ src/App/pages/Product/index.jsx
@@ -1,11 +1,35 @@
 import { Footer } from "../../components/Footer"
-import { ProductPageHeader } from "../../components/Header/ProductPageHeader/ProductPageHeader"
+import { ProductPageHeader } from "../../components/Header/ProductPageHeader/ProductPageHeader";
+import style from './style.module.css';
+import {ReactComponent as SearchIcon} from "../../assets/Icons/search.svg";
+import { CustomButton } from "../../components/Button";
+import { BagProductsList } from "../../components/BagProducts";
 
 export const ProductPage = () => {
-    return (
-        <div>
-            <ProductPageHeader />
-            <Footer />
+    return (<section className={style.wrapper}>
+        <ProductPageHeader className={style.menu} />
+        <div className={style.specialoffer}>
+            <h1>Special Offer Get 20% off now</h1>
+            <a href="#/">Get it now</a>
         </div>
+        <div className={style.seachsection}>
+            <div className={style.search}>
+                <SearchIcon className={style.icon} />
+                <input className={style.input}
+                    type="search"
+                    name="Search"
+                    id="search"
+                    placeholder="Search products and categories" />
+            </div>
+            <CustomButton className={style.button} onClick={()=>{}}>Search</CustomButton>
+        </div>
+        <div className={style.product}>
+            <BagProductsList />
+           
+        </div>
+        <div>
+        <Footer />
+        </div>
+        </section>
     )
 }
